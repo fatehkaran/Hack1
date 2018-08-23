39371c6afb7b879f4b0d1612bde1e7d163fae54b
Fateh Karan Singh Sandhu <fatehkaran@huskers.unl.edu>
2018-08-23 11:33:05 -0500

    Fateh

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Fateh Karan Singh Sandhu
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Name: Fateh Karan Singh Sandhu\nMajor: Computer Science \n");
+
+   // The print function prints my name and my Major
+
+  return 0;
+}
